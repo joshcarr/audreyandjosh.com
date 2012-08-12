8619a35fc0b40747024cd844fecf14806b474b53
Josh Carr <josh@barbariangroup.com>
2012-08-11 21:18:10 -0700

    adjusts a bunch of stuff

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 /* Author:
 
 */
+
+// execute function when the page is ready
+Zepto(function($){
+
+  $( window ).on( 'resize', function () {
+    $( '.container').height( $(window).height() );
+  } ).trigger('resize');
+
+});
