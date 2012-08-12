f2b79804921313dff1686cc37733df15c341d77f
Josh Carr <josh@barbariangroup.com>
2012-08-11 18:03:03 -0700

    styles more with media queries.

--- grunt.js
+++ grunt.js
@@ -1,116 +1,205 @@
-/*global module:false*/
+// This is the main application configuration file.  It is a Grunt
+// configuration file, which you can learn more about here:
+// https://github.com/cowboy/grunt/blob/master/docs/configuring.md
+//
 module.exports = function(grunt) {
 
-  // Project configuration.
+  var SRC_CSS   = 'css/src/',
+      SRC_JS    = 'js/src/',
+      BUILD_CSS = 'css/bin/',
+      BUILD_JS  = 'js/bin/';
+
   grunt.initConfig({
-    // the staging directory used during the process
-    staging: 'intermediate',
-    // final build output
-    output: 'publish',
-    // filter any files matching one of the below pattern during mkdirs task
-    // the pattern in the .gitignore file should work too.
-    exclude: '.git* build/** node_modules/** grunt.js package.json *.md'.split(' '),
-    mkdirs: {
-      staging: '<config:exclude>'
-    },
-    // concat css/**/*.css files, inline @import, output a single minified css
-    css: {
-      'css/style.css': ['css/**/*.css']
-    },
-    // Renames JS/CSS to prepend a hash of their contents for easier
-    // versioning
-    rev: {
-      js: 'js/**/*.js',
-      css: 'css/**/*.css',
-      img: 'img/**'
-    },
-    // update references in html to revved files
-    usemin: {
-      files: ['**/*.html']
-    },
-    // html minification
-    html: '<config:usemin>',
-    // Optimizes JPGs and PNGs (with jpegtran & optipng)
-    img: {
-      dist: '<config:rev.img>'
-    },
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    
-    pkg: '<json:package.json>',
-    meta: {
-      banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
-        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-        '<%= pkg.homepage ? "* " + pkg.homepage + "\n" : "" %>' +
-        '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-        ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */'
-    },
+
+    // The clean task ensures all files are removed from the dist/ directory so
+    // that no files linger from previous builds.
+    clean: ["dist/"],
+
+    // The lint task will run the build configuration and the application
+    // JavaScript through JSHint and report any errors.  You can change the
+    // options for this task, by reading this:
+    // https://github.com/cowboy/grunt/blob/master/docs/task_lint.md
     lint: {
-      files: ['grunt.js', 'js/**/*.js', 'test/**/*.js']
+      files: [
+        "build/config.js", "app/**/*.js"
+      ]
     },
-    qunit: {
-      files: ['test/**/*.html']
-    },
-    concat: {
-      dist: {
-        src: ['js/plugins.js', 'js/main.js'],
-        dest: 'js/audreyandjosh.com-0.1.0.js'
-      }
-    },
-    min: {
-      dist: {
-        src: 'js/audreyandjosh.com-0.1.0.js',
-        dest: 'js/main.js'
-      }
-    },
+
+    // The jshint option for scripturl is set to lax, because the anchor
+    // override inside main.js needs to test for them so as to not accidentally
+    // route.
     jshint: {
       options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
+        scripturl: true
       }
     },
-    uglify: {},
-    rjs: {
-      modules: [{
-        name: 'main',
-      }],
-      dir: 'publish/js',
-      appDir: 'js',
-      baseUrl: './',
-      pragmas: {
-        doExclude: true
+
+    // The jst task compiles all application templates into JavaScript
+    // functions with the underscore.js template function from 1.2.4.  You can
+    // change the namespace and the template options, by reading this:
+    // https://github.com/tbranyen/build-tasks/tree/master/jst
+    //
+    // The concat task depends on this file to exist, so if you decide to
+    // remove this, ensure concat is updated accordingly.
+    jst: {
+      "dist/debug/templates.js": [
+        "app/templates/**/*.html"
+      ]
+    },
+
+    // This task uses the MinCSS Node.js project to take all your CSS files in
+    // order and concatenate them into a single CSS file named index.css.  It
+    // also minifies all the CSS as well.  This is named index.css, because we
+    // only want to load one stylesheet in index.html.
+    mincss: {
+      "assets/css/bin/base.css": [
+        "assets/css/bin/base.css"
+      ]
+    },
+
+    // Takes the built require.js file and minifies it for filesize benefits.
+    min: {
+      "dist/release/build.js": [
+        "dist/debug/build.js"
+      ]
+    },
+
+    // Running the server without specifying an action will run the defaults,
+    // port: 8080 and host: 127.0.0.1.  If you would like to change these
+    // defaults, simply add in the properties `port` and `host` respectively.
+    //
+    // Changing the defaults might look something like this:
+    //
+    // server: {
+    //   host: "127.0.0.1", port: 9001
+    //   debug: { ... can set host and port here too ...
+    //  }
+    //
+    //  To learn more about using the server task, please refer to the code
+    //  until documentation has been written.
+    server: {
+      files: { "favicon.ico": "favicon.ico" },
+
+      debug: {
+        files: { "favicon.ico": "favicon.ico" },
+
+        folders: {
+          "app": "dist/debug",
+          "assets/js/libs": "dist/debug"
+        }
       },
-      skipModuleInsertion: false,
-      optimizeAllPluginResources: true,
-      findNestedDependencies: true
+
+      release: {
+        // These two options make it easier for deploying, by using whatever
+        // PORT is available in the environment and defaulting to any IP.
+        host: "0.0.0.0",
+        port: process.env.PORT || 8000,
+
+        files: { "favicon.ico": "favicon.ico" },
+
+        folders: {
+          "app": "dist/release",
+          "assets/js/libs": "dist/release",
+          "assets/css": "dist/release"
+        }
+      }
+    },
+
+    // This task uses James Burke's excellent r.js AMD build tool.  In the
+    // future other builders may be contributed as drop-in alternatives.
+    // requirejs: {
+    //   // Include the main configuration file
+    //   mainConfigFile: "app/config.js",
+
+    //   // Output file
+    //   out: "dist/debug/require.js",
+
+    //   // Root application module
+    //   name: "config",
+
+    //   // Do not wrap everything in an IIFE
+    //   wrap: false
+    // },
+    requirejs: {
+      compile: {
+        options: {
+          mainConfigFile: "app/config.js",
+          out: "dist/debug/require.js",
+          name: "config",
+          wrap: false
+        }
+      }
+    },
+    less: {
+      css: {
+        src: SRC_CSS + 'base.less',
+        dest: SRC_CSS + 'base.css'
+      }
+    },
+    watch: {
+      // js: {
+      //   files: '<config:concat.js.src>',
+      //   tasks: 'lint concat:js'
+      // },
+      less: {
+        files: '<config:concat.less.src>',
+        tasks: 'concat:less less concat:css'
+      }
+      
+    },
+    concat: {
+      
+      less: {
+        src: [SRC_CSS + 'vendor/bootstrap.less',
+              SRC_CSS + 'sprites.less',
+              SRC_CSS + 'styles.less',
+              SRC_CSS + 'buttons.less',
+              SRC_CSS + 'about.less',
+              SRC_CSS + 'mediaqueries.less'],
+        dest: SRC_CSS + 'base.less'
+      },
+      
+      css: {
+        src: [SRC_CSS + '*.css'],
+        dest: BUILD_CSS + 'base.css'
+      },
+
+      // The concatenate task is used here to merge the almond require/define
+      // shim and the templates into the application code.  It's named
+      // dist/debug/require.js, because we want to only load one script file in
+      // index.html.
+      js: {
+        src: ["assets/js/libs/almond.js",
+              "dist/debug/templates.js",
+              "dist/debug/require.js"],
+        dest: 'dist/debug/build.js'
+      }
     }
+
+
   });
 
-  
-  // in rjs setup, the concat and min task are overriden to use rjs optimizr
-  grunt.renameTask('concat', '_concat').registerTask('concat', 'rjs (noop)', function() {
-    grunt.log.writeln('the concat in rjs setup is a noop, rjs optimizer somewhat replace js concatenation');
-  });
-  grunt.renameTask('min', '_min').registerTask('min', 'rjs');
-  
+  grunt.loadNpmTasks('grunt-less');
+  grunt.loadNpmTasks('grunt-css');
+  grunt.loadNpmTasks('grunt-contrib');
 
-  // uncomment this line if you're using the build script as a grunt plugin
-  // it should be installed locally, even better if put in your package.json's
-  // dependency
-  //
-  // grunt.loadNpmTasks('node-build-script');
+
+  grunt.registerTask("prepare-less", "concat:less less concat:css");
+
+  // The default task will remove all contents inside the dist/ folder, lint
+  // all your code, precompile all the underscore templates into
+  // dist/debug/templates.js, compile all the application code into
+  // dist/debug/require.js, and then concatenate the require/define shim
+  // almond.js and dist/debug/templates.js into the require.js file.
+  grunt.registerTask("default", "prepare-less clean lint jst requirejs concat:js");
+
+  // The debug task is simply an alias to default to remain consistent with
+  // debug/release.
+  grunt.registerTask("debug", "default");
+
+  // The release task will run the debug tasks and then minify the
+  // dist/debug/require.js file and CSS files.
+  grunt.registerTask("release", "debug min mincss");
+
+
 };
